485266148b46adc0a92765941a4650197d0d7195
Krishna Deshmukh <krishnadeshmukh2844@gmail.com>
2026-07-08 23:24:16 +0530

    added React setup and Navbar component

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./components/Navbar"
+
+function App() {
+  return (
+    <div>
+    <Navbar/>
+      <h1>Welcome to TuitionConnect</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+function Navbar(){
+    return(<nav>
+        <h2>TuitionConnect</h2>
+        <ul>
+           <li>Home</li>
+           <li>Find Tutors</li>
+           <li>Login</li>
+           <li>Sign Up</li>
+
+        </ul>
+    </nav>)
+}
+export default Navbar
